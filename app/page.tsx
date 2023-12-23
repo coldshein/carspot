9a1b5b9acab488ae2619c27d59a12b8b89fc2201
Roman <coldshein@gmail.com>
2023-12-23 22:44:23 +0200

    builded

--- app/page.tsx
+++ app/page.tsx
@@ -27,8 +27,8 @@ export default async function Home() {
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {allCars?.map((car) => (
-                <CarCard car={car} />
+              {allCars?.map((car, index) => (
+                <CarCard car={car} key={car.title + index} />
               ))}
             </div>
           </section>
